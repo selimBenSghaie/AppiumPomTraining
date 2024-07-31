6014a8964f5cdc83de1d68a7ef0d1d76a49b4131
selimBenSghaie <selimbensghaier@gmail.com>
2024-07-31 22:39:19 +0100

    Training Appium Version 0.1

---
+++ AppiumProjectWithPOM/src/main/java/com/pom/base/Base.java
@@ -0,0 +1,13 @@
+package com.pom.base;
+
+import java.io.File;
+
+public class Base {
+
+	public void configCapability() {
+		
+		//initialiser le chemin de l'application .apk
+		
+		File app = new File("src/test/resources/apps/com.gero.newpass_12.apk");
+	}
+}
